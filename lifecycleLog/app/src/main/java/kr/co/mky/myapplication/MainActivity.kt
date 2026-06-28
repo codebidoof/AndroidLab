46e2e983d37036fbd75e7546b6b43a16e9b5afc4
princehw03 <princehw03@gmail.com>
2026-06-29 02:15:13 +0900

    🏗️ chore(project): 프로젝트 환경 설정 및 IDE 구성 파일 추가
    - IDE 프로젝트 메타데이터 및 Android 시스템 설정 파일 추가
    - Gradle 로컬 환경 및 JDK 21 기반의 컴파일러 설정 정의
    - 가상 디바이스 스트리밍 캐시 및 배포 타겟 선정 정보 추가
    - MainActivity.kt에 PersistableBundle 임포트 추가

--- lifecycleLog/app/src/main/java/kr/co/mky/myapplication/MainActivity.kt
+++ lifecycleLog/app/src/main/java/kr/co/mky/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package kr.co.mky.myapplication
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
